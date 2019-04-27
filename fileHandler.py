4863257b09fabd174dde170f9f9ecabdeb0765e5
Peter Hagen <phgn@protonmail.ch>
2019-04-27 17:58:24 +0200

    Add image display

--- fileHandler.py
+++ fileHandler.py
@@ -5,22 +5,26 @@ import subprocess
 class FileHandler:
     def __init__(self):
         self.count = 0
+        self.displayProcess = None
 
     def setSeriesName(self, name):
         self.baseName = name
 
     def setSeriesDate(self, year):
+        self.count = 0
         self.baseDate = str(year) + ":01:01 00:{:02d}:{:02d}"
 
     def _getRunningDate(self):
-        self.count = 0
-
         seconds = self.count % 60
         hours = self.count // 60
 
         return self.baseDate.format(hours, seconds)
 
     def processNewFile(self, path):
+        # end showing old image
+        if (self.displayProcess):
+            self.displayProcess.kill()
+
         # rename file
         directory = os.path.dirname(path) + "/"
         newName = directory + self.baseName + "_{}.jpg".format(self.count)
@@ -32,7 +36,8 @@ class FileHandler:
                 "-DateTimeOriginal='{}'".format(self._getRunningDate()),
                 newName])
 
-        # displays
-        # subprocess.call(["feh", "--scale-down", newName])
+        # display new image
+        self.displayProcess = subprocess.Popen(
+            ["feh", "--scale-down", newName])
 
         self.count += 1
